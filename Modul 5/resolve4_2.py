dee2fd8d187519e4720553cb8a835196504b349b
dez08 <110081817+dez08@users.noreply.github.com>
2024-06-25 14:29:20 +0800

    Modul_5_resolve4_2

---
+++ Modul 5/resolve4_2.py
@@ -0,0 +1,12 @@
+# Домашнее задание по уроку "Различие атрибутов класса и экземпляра"
+
+class Building:
+    total = 1
+
+    def __init__(self, total):
+        self.total = total
+
+
+for i in range(40):
+    locals()['bld' + str(i)] = Building(i + 1)
+    print(f'Объект № {locals()['bld' + str(i)].total} в классе "Building"')
