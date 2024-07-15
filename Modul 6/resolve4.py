16a80c926c0dad05bc3f314f8938ebc3f3edcd08
dez08 <110081817+dez08@users.noreply.github.com>
2024-07-15 19:54:32 +0800

    Update resolve4.py

--- Modul 6/resolve4.py
+++ Modul 6/resolve4.py
@@ -8,7 +8,6 @@ class Figure:
         self.__color = __color
         self.filled = True
 
-
     def get_color(self):
         return self.__color
 
@@ -38,12 +37,19 @@ class Figure:
         return sum(self.__sides)
 
     def set_sides(self, *new_sides):
-         if self.__is_valid_sides(*new_sides):
-            self.__sides = [*new_sides]
+        if self.sides_count > 1:
+            a = self.__sides
+            b = []
+            for _ in range(self.sides_count):
+                b.append(a)
+            self.__sides = b
+        else:
+            if self.__is_valid_sides(*new_sides):
+                self.__sides = [*new_sides]
 
 
 class Circle(Figure):
-    def __init__(self,__color, __sides):
+    def __init__(self, __color, __sides):
         super().__init__(__color, __sides)
         self.sides_count = 1
         self.__radius = __sides / (2 * pi)
@@ -68,29 +74,24 @@ class Cube(Figure):
     def __init__(self, __color, __sides):
         super().__init__(__color, __sides)
         self.sides_count = 12
-        a = __sides
-        b = []
-        for _ in range(self.sides_count):
-            b.append(a)
-        Figure.__sides = b
 
     def get_volume(self):
-        return self.__sides[0] ** 3
+        return self._Figure__sides[0] ** 3
 
 
-circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
 # Проверка на изменение цветов:
-circle1.set_color(55, 66, 77) # Изменится
+circle1.set_color(55, 66, 77)  # Изменится
 print(circle1.get_color())
-cube1.set_color(300, 70, 15) # Не изменится
+cube1.set_color(300, 70, 15)  # Не изменится
 print(cube1.get_color())
 
 # Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
 print(cube1.get_sides())
-circle1.set_sides(15) # Изменится
+circle1.set_sides(15)  # Изменится
 print(circle1.get_sides())
 
 # Проверка периметра (круга), это и есть длина:
