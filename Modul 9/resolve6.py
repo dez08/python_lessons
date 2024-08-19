d304fd5320ed0f462bcb9957e10434c18f394b5a
dez08 <110081817+dez08@users.noreply.github.com>
2024-08-19 09:21:54 +0800

    Add files via upload

---
+++ Modul 9/resolve6.py
@@ -0,0 +1,11 @@
+# Домашнее задание по теме "Генераторы"
+
+def all_variants(text):
+    for i in range(len(text)):
+        for j in range(i, len(text)):
+            yield text[i:j + 1]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
