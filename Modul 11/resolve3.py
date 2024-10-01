bd7cf80dfab3301adfc2a33427f16802abd2e65c
dez08 <110081817+dez08@users.noreply.github.com>
2024-10-01 08:37:55 +0800

    Update resolve3.py

--- Modul 11/resolve3.py
+++ Modul 11/resolve3.py
@@ -4,7 +4,8 @@ def introspection_info(obj):
     obj_type = type(obj).__name__
     obj_attributes = [attributes for attributes in dir(obj) if not callable(getattr(obj, attributes))]
     obj_methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    obj_module = obj.__module__
+    obj_module = obj.__class__.__module__
+
 
     result = {'type': obj_type,
               'attributes': obj_attributes,
@@ -24,7 +25,14 @@ class MyClass:
 
 
 obj = MyClass()
-number_info = introspection_info(obj)
+number_info_1 = introspection_info(42)
+number_info_2 = introspection_info(obj)
 
-for key, value in number_info.items():
+
+for key, value in number_info_1.items():
+    print(f"{key}: {value}")
+
+print()
+
+for key, value in number_info_2.items():
     print(f"{key}: {value}")
