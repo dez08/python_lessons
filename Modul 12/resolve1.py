9c479ac85f66c2f819497b4903a31e1e332491f9
dez08 <110081817+dez08@users.noreply.github.com>
2024-10-01 14:49:52 +0800

    Add files via upload

---
+++ Modul 12/resolve1.py
@@ -0,0 +1,44 @@
+# Домашнее задание по теме "Простые Юнит-Тесты"
+
+import unittest
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        run_1 = Runner('Vlad')
+        for _ in range(10):
+            run_1.walk()
+        self.assertEqual(run_1.distance, 50)
+
+    def test_run(self):
+        run_2 = Runner('Ivan')
+        for _ in range(10):
+            run_2.run()
+        self.assertEqual(run_2.distance, 100)
+
+    def test_challenge(self):
+        run_1 = Runner('Vlad')
+        run_2 = Runner('Ivan')
+        for _ in range(10):
+            run_1.walk()
+            run_2.run()
+        self.assertNotEqual(run_1.distance, run_2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
