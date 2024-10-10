7366fc5e34c06d23cf45497ccb1feab249f62134
dez08 <110081817+dez08@users.noreply.github.com>
2024-10-10 14:54:30 +0800

    Update resolve2.py

--- Modul 13/resolve2.py
+++ Modul 13/resolve2.py
@@ -1,4 +1,4 @@
-# Домашнее задание по теме "Методы отправки сообщений".
+# Домашнее задание по теме "Хендлеры обработки сообщений".
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
