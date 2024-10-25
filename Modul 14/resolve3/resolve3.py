79b332db09676a2a8c35bd66f3ee4704e14f996e
dez08 <110081817+dez08@users.noreply.github.com>
2024-10-25 16:25:42 +0800

    Update resolve3.py

--- Modul 14/resolve3/resolve3.py
+++ Modul 14/resolve3/resolve3.py
@@ -7,7 +7,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7587294616:AAFKistZE_RYK_50FJ3DenhXwZ1QS_EMVdY'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
