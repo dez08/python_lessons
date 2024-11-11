b5297b9b11f1029dc95db85c53661acf4c0cfb7c
dez08 <110081817+dez08@users.noreply.github.com>
2024-11-11 12:37:09 +0800

    Update resolve_3.py

--- Modul 16/resolve_3.py
+++ Modul 16/resolve_3.py
@@ -19,22 +19,22 @@ async def welcome() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def welcome_id_user(username: Annotated[str,Path(min_length=5,max_length=20, description= 'Enter username', example='Piter')],
-                          age: int = Path(ge=18,le=120,description='Enter age', example='19')) -> str:
+async def welcome_id_user(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
+                          age: int = Path(ge=18, le=120, description='Enter Age', example='24')) -> str:
     user_id = str(int(max(users,key=int))+1)
     users[user_id] = f'Имя: {username}, возраст {age}'
     return f'User {user_id} is registered'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def change_id_user(user_id: int = Path(ge=1,le=100,description='Enter User ID', example='1')
-                         , username: str = Path(min_length=5,max_length=20, description= 'Enter username', example='Piter'),
-                          age: int = Path(ge=18,le=120,description='Enter age', example='19')) -> str:
+async def change_id_user(user_id: int = Path(ge=1, le=100,description='Enter User ID', example='1'),
+                         username: str = Path(min_length=5,max_length=20, description= 'Enter username', example='UrbanUser'),
+                         age: int = Path(ge=18, le=120,description='Enter age', example='24')) -> str:
     users[user_id] = f'Имя: {username}, возраст {age}'
     return f'User {user_id} has been updated'
 
 
 @app.delete('/user/{user_id}')
-async def del_user(user_id: int = Path(ge=1,le=100,description='Enter User ID', example='1')) -> str:
+async def del_user(user_id: int = Path(ge=1, le=100,description='Enter User ID', example='1')) -> str:
     users.pop(str(user_id))
     return f'User {user_id} has been deleted'
