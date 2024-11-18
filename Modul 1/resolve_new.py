de21f895833f8670eeb2008308ffe4fafa581eb0
dez08 <110081817+dez08@users.noreply.github.com>
2024-11-18 21:37:42 +0800

    Create resolve_new.py

---
+++ Modul 1/resolve_new.py
@@ -0,0 +1,18 @@
+# Практическое задание по уроку "Базовые структуры данных"
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+if 9.99 > 9.98 and 1000 < 1000.1:
+    print(True)
+
+# 3rd program
+result_1 = 2 * 2 + 2
+result_2 = 2 * (2 + 2)
+print(result_1)
+print(result_2)
+print(result_1 == result_2)
+
+# 4th program
+print((int(float('123.456') * 10)) % 10)
